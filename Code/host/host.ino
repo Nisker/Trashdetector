66c36e919874b2eb4227666c184530cf8910db77
Benjamin Nisker <Benjamin-nisker@hotmail.com>
2020-12-01 15:38:39 +0100

    forgot a file

--- Code/host/host.ino
+++ Code/host/host.ino
@@ -2,42 +2,46 @@
 #include <SPI.h>
 #include <RH_RF95.h>
 
-// Singleton instance of the radio driver
 RH_RF95 rf95;
 
 int led = 9;
 
-void setup() 
+void setup()
 {
-  pinMode(led, OUTPUT);     
+  pinMode(led, OUTPUT);
   Serial.begin(115200);
   while (!Serial) ; // Wait for serial port to be available
   if (!rf95.init())
-    Serial.println("init failed");  
+    Serial.println("init failed");
   rf95.setTxPower(20, false);
 }
-
+unsigned long last = 0;
 void loop()
 {
+  if (millis() - last > 4000) {
+    last = millis();
+    uint8_t duta[] = "B";
+    rf95.send(duta, sizeof(duta));
+    rf95.waitPacketSent();
+  }
+
   if (rf95.available())
   {
-    // Should be a message for us now   
+    // Should be a message for us now
     uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
     uint8_t len = sizeof(buf);
     if (rf95.recv(buf, &len))
     {
       digitalWrite(led, HIGH);
-      //RH_RF95::printBuffer("request: ", buf, len);
-      float i = ((buf[1]<<8)+buf[2]);
-      i=i/10;
-      Serial.printf("Got %3.1fcm. from device: %d. RSSI: %d\n", i, buf[0], rf95.lastRssi());
-  
-      
+      float i = ((buf[1] << 8) + buf[2]);
+      i = i / 10;
+      i > 500 ? Serial.printf("Blocked sensor. from device: %d. RSSI: %d\n", buf[0], rf95.lastRssi()) : Serial.printf("Got %3.1fcm. from device: %d. RSSI: %d\n", i, buf[0], rf95.lastRssi());
+
       // Send a reply
       uint8_t data[] = "A";
       rf95.send(data, sizeof(data));
       rf95.waitPacketSent();
-       digitalWrite(led, LOW);
+      digitalWrite(led, LOW);
     }
     else
     {
