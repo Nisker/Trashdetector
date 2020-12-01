c390acccbb322e026bc12101039c1250aca01c5a
Benjamin Nisker <Benjamin-nisker@hotmail.com>
2020-12-01 14:05:12 +0100

    added master initates communication

--- Code/Lora_rf95_klient/Lora_rf95_klient.ino
+++ Code/Lora_rf95_klient/Lora_rf95_klient.ino
@@ -8,9 +8,9 @@ RH_RF95 rf95;
 //definerer afstandsensor pins
 AfstandsSensor afstandssensor(3, 4);
 
-void setup() 
+void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) ;
   if (!rf95.init())
     Serial.println("init failed");
@@ -20,30 +20,28 @@ void setup()
 
 }
 
-void lorasend (double i){
+void lorasend (double i) {
   Serial.println("Sending to rf95_server");
   // Send a message to rf95_server
   int s = (int) i;
-uint8_t data [3];
-  data[1] = (s>>8)&0xff;
-  data[2] = (s)&0xff;
+  uint8_t data [3];
+  data[1] = (s >> 8) & 0xff;
+  data[2] = (s) & 0xff;
   data[0] = 69;
   rf95.send(data, sizeof(data));
-  
+
   rf95.waitPacketSent();
   // Now wait for a reply
   uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
   uint8_t len = sizeof(buf);
 
   if (rf95.waitAvailableTimeout(3000))
-  { 
-    // Should be a reply message for us now   
+  {
+    // Should be a reply message for us now
     if (rf95.recv(buf, &len))
-   {
+    {
       Serial.print("got reply: ");
       Serial.println((char*)buf);
-//      Serial.print("RSSI: ");
-//      Serial.println(rf95.lastRssi(), DEC);    
     }
     else
     {
@@ -54,18 +52,22 @@ uint8_t data [3];
   {
     Serial.println("No reply, is rf95_server running?");
   }
-  delay(4000);
 }
 
+void wakeUp() {
+  uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+  if (rf95.recv(buf, &len)) {
+    if (!strcmp(buf, "B")) {
+      double i = afstandssensor.afstandCM();
+      Serial.print("afstand: ");
+      Serial.println(i);
+      lorasend(i * 10);
+    }
+  }
+}
 
-void loop(){
+void loop() {
+  if (rf95.available()) wakeUp();
 
-double i = afstandssensor.afstandCM();
-
-Serial.print("afstand: ");
-Serial.println(i);
-
-lorasend(i*10);
-
-  
 }
